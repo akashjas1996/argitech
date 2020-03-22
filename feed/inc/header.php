d4ad61b28ae81aa7b531ee3e0ab6e3b181cc25bf
Akash Jaiswal <akashjas1996@gmail.com>
2020-03-22 19:00:09 +0530

    Update feed/inc/header.php

--- feed/inc/header.php
+++ feed/inc/header.php
@@ -1,6 +1,6 @@
 <!--Navbar -->
 <nav class="mb-1 navbar navbar-expand-lg navbar-dark green accent-9">
-  <a class="navbar-brand" href="#">Registration</a>
+  <a class="navbar-brand" href="../reg/">Registration</a>
   <a class="navbar-brand" href="../ben/">Beneficiary List</a>
   <a class="navbar-brand" href="#">Agritech</a>
   <a class="navbar-brand" href="#">Agritech</a>
