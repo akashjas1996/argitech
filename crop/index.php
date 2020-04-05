c1f310751f36a2cf2ed043c9d01de7a8c80879f6
Akash Jaiswal <akashjas1996@gmail.com>
2020-04-06 04:40:45 +0530

    Update crop/index.php

--- crop/index.php
+++ crop/index.php
@@ -45,7 +45,6 @@
             else{
                 $query_crop_area = "SELECT * FROM crop_cultivation WHERE name='$crop_name'";
             }
-            
         }
         $res_crop_area = mysqli_query($link, $query_crop_area);
         $crop_sum=0;
@@ -55,6 +54,48 @@
         array_push($dataPointsCrops1, array("y" => "$crop_sum", "legendText" => "$crop_name", "label" => "$crop_name"));
     }
 	?>
+
+
+
+    <?php 
+        if(isset($_GET['field'])){
+            $val = (int)$_GET['field'];
+            $val = $val*1;
+            $query_crop = "SELECT * FROM crop_types WHERE crop_id='$val'";
+            $res_crop = mysqli_query($link, $query_crop);
+            $row_crop = mysqli_fetch_assoc($res_crop);
+            $crop_name = $row_crop['crop_name'];
+            echo $crop_name;
+            // echo $val;
+        }
+    $dataPointsIncome = array();
+    $query_crops = "SELECT DISTINCT name FROM crop_cultivation";
+    $res_crop = mysqli_query($link, $query_crops);
+    while($row_crop = mysqli_fetch_assoc($res_crop)){
+        $crop_name = $row_crop['name'];
+        if(isset($_GET['time'])){
+            if($_GET['time']=='baseline')
+            {
+                $query_crop_area = "SELECT * FROM crop_cultivation WHERE name='$crop_name' AND bsl_crop='0'";
+            }
+            else if($_GET['time']=='2019-20'){
+                $query_crop_area = "SELECT * FROM crop_cultivation WHERE name='$crop_name' AND bsl_crop='1' AND intv_year='2019-20'";
+            }
+            else{
+                $query_crop_area = "SELECT * FROM crop_cultivation WHERE name='$crop_name'";
+            }
+        }
+        $res_crop_area = mysqli_query($link, $query_crop_area);
+        $crop_income=0;
+        while($row_crop_area=mysqli_fetch_assoc($res_crop_area)){
+            $crop_income+=  $row_crop_area['net_income'];
+        }
+        array_push($dataPointsIncome, array("y" => "$crop_income", "legendText" => "$crop_name", "label" => "$crop_name"));
+    }
+    ?>
+
+
+
 </h1>
 
 <center><h2>Crop Report</h2></center>
@@ -72,69 +113,16 @@
         <div id="chartContainerCrops"></div>
     </div>
 </div>
-
+<br><br>
 <div class="row">
-    <!-- <div class="col-lg-4">
-        <div id="chartContainerCategories"></div>
-    </div> -->
-    <div class="col-lg-5">
-        <div id="chartContainer"></div>
+    <div class="col-lg-12">
+        <div id="chartContainerIncome"></div>
     </div>
-     
 </div>
 
 
-<?php
-    $dataPoints = array(
-	array("x" => 1325356200000, "y" => 2600),
-	array("x" => 1328034600000, "y" => 3800),
-	array("x" => 1330540200000, "y" => 4300),
-	array("x" => 1333218600000, "y" => 2900),
-	array("x" => 1335810600000, "y" => 4100),
-	array("x" => 1338489000000, "y" => 4500),
-	array("x" => 1341081000000, "y" => 8600),
-	array("x" => 1343759400000, "y" => 6400),
-	array("x" => 1346437800000, "y" => 5300),
-	array("x" => 1349029800000, "y" => 6000)
-    );
-      $dataPointsCategories = array();
-
-   
-        
-
-?>
-
 
 <script type="text/javascript">
-   
-
-    // $(function () {
-    //     var chart = new CanvasJS.Chart("chartContainerCategories", {
-    //         theme: "light2",
-    //         title: {
-    //             text: "Categories"
-    //         },
-    //         animationEnabled: true,
-    //         legend: {
-    //             fontSize: 20,
-    //             fontFamily: "Helvetica"
-    //         },
-    //         theme: "light2",
-    //         data: [
-    //         {
-    //             type: "doughnut",
-    //             indexLabelFontFamily: "Garamond",
-    //             indexLabelFontSize: 20,
-    //             indexLabel: "{label} {y}%",
-    //             startAngle: -20,
-    //             showInLegend: true,
-    //             toolTipContent: "{legendText} {y}%",
-    //             dataPoints: <?php echo json_encode($dataPointsCategories, JSON_NUMERIC_CHECK); ?>
-    //         }
-    //         ]
-    //     });
-    //     chart.render();
-    // });
 
     $(function () {
         var chart = new CanvasJS.Chart("chartContainerCrops", {
@@ -147,6 +135,23 @@
             {
                 type: "column",                
                 dataPoints: <?php echo json_encode($dataPointsCrops1, JSON_NUMERIC_CHECK); ?>
+            }
+            ]
+        });
+        chart.render();
+    });
+
+     $(function () {
+        var chart = new CanvasJS.Chart("chartContainerIncome", {
+            theme: "light2",
+            animationEnabled: true,
+            title: {
+                text: "Crops Vs Income(in ₹)"
+            },
+            data: [
+            {
+                type: "column",                
+                dataPoints: <?php echo json_encode($dataPointsIncome, JSON_NUMERIC_CHECK); ?>
             }
             ]
         });
