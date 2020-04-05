b72eea9342abe5b4189fb8ead767c7477b7644f9
Akash Jaiswal <akashjas1996@gmail.com>
2020-04-06 05:19:50 +0530

    Update location/index.php

--- location/index.php
+++ location/index.php
@@ -177,7 +177,7 @@ https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
      $query_gp1 = "SELECT DISTINCT gp FROM family WHERE state='Jharkhand' AND dist='East Singhbhum'";
      $res_gp1 = mysqli_query($link, $query_gp1);
      while($row_gp1 = mysqli_fetch_assoc($res_gp1)){
-       echo '<a href="../report/?GP='. echo $row_gp1['gp'].'">';
+      echo '<a href="../report/?GP='.$row_gp1['gp'].'"';
       echo $row_gp1['gp'];
       echo '</a>';
       echo '<br>';
