ac54b7352b3a061f6ec8db6a4df3de511fbf485f
Akash Jaiswal <akashjas1996@gmail.com>
2020-04-17 19:41:41 +0530

    Update index/index.php

--- index/index.php
+++ index/index.php
@@ -163,6 +163,7 @@ $dataPoints_2019_20 = array();
         $sumbaseline = $sum_crop+$sum_livestock+$sum_allied+$sum_daliywage+$sum_enterprise;
         $final_sum+=$sumbaseline;
         echo $final_sum;
+        echo '<br>';
 
     }
     // echo '2019-20 <br>'.$sumbaseline;
